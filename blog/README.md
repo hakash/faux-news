5ab995afbf5ba17c5af9667a4b463a69c4661aa3
Morten Olsrud <post@dittdesign.com>
2019-11-09 14:30:14 +0100

    Merged restructuring

--- blog/README.md
+++
@@ -1,4 +0,0 @@
-# Welcome
-
-This is a demo project for Java 11 with the Spring Boot framework.
-
--- blog/src/main/resources/static/js/blog.js
+++
@@ -1,3 +0,0 @@
-function viewArticle(el){
-	window.location.href = "/article/" + el.getAttribute("data-article_id");
-}
